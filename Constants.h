8a9390edf60ec49c08c159b89894b3419096d73f
Giallo <th3g91@gmail.com>
2016-07-13 18:53:35 +0200

    Implementato draw dei marker probabili ma non correttamente classificati. Aggiunto file Constants.h

---
+++ Constants.h
@@ -0,0 +1,4 @@
+#pragma once
+#define WIDTH 640 /* Do Not Change, this is for CamParams*/
+#define HEIGHT 480 /* Do Not Change, this is for CamParams*/
+#define M_PI 3.14159265358979323846  /* pi */
