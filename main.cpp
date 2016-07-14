2f5c88237cb85ffe011d54866cd5e5aff3c0d1f0
Giallo <th3g91@gmail.com>
2016-07-14 14:50:44 +0200

    Fixato errore di compilazione

--- main.cpp
+++ main.cpp
@@ -11,7 +11,7 @@
 #include <fstream>
 //#include "NaoUtils.h"
 #include "detection.h"
-#include "TheWalkingNao.h"
+//#include "TheWalkingNao.h"
 #include "Classiwekation.h"
 #include "Constants.h"
 #include "FeatExtract.h"
